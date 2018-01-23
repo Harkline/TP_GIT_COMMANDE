02f1af3e1823fbcf2192360aa2ac63c3f75aff54
Yanis <yanislevesque@hotmail.fr>
2018-01-23 15:59:16 +0100

    L'afichage du prenom et de la classe gere le genre

--- Heros.java
+++ Heros.java
@@ -1,20 +1,25 @@
 public class Heros{
 	private String chNom;
 	private String chClasse; 
+	private String chGenre;
 	
-	public Heros(String parNom, String parClasse){
+	public Heros(String parNom, String parClasse,String parGenre){
 		chNom=parNom;
 		chClasse=parClasse;
+		chGenre=parGenre;
 	}
 	
 	public String toString(){
-		return chNom +" "+  chClasse  ;
+		if (chGenre=="m") 
+			return chNom +" le "+ chClasse;
+		return chNom +" la "+ chClasse;
 	} //toString() 
 	
 	public static void main (String[] args){
-	Heros bob;
-	bob=new Heros ("Bob", "le magicien");
-	
-	System.out.println(bob.toString());
+		Heros bob;								    //La création d'un personnage en deux lignes !	
+		bob = new Heros ("Bob","magicien","m");
+		Heros zoe = new Heros("Zoe","barbare","f"); //En une seule ligne !
+		System.out.println(bob.toString());
+		System.out.println(zoe.toString());
 	} 
 }
