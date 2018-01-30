f320778dac13b3d868f2fd9aadbf82a98989b2b3
Berzok <34751752+Berzok@users.noreply.github.com>
2018-01-30 16:43:37 +0100

    Add files via upload

--- FondCarac.java
+++ FondCarac.java
@@ -13,26 +13,18 @@ public class FondCarac extends JPanel
 		{
 		Heros chHeros;
 		chHeros = parHeros;
-		for(int i=0; i<6; i++)
-			{
-			lesFonds[i] = new JPanel();
-			lesFonds[i].setLayout(new BoxLayout(lesFonds[i], BoxLayout.LINE_AXIS));
-			}
 		
-			{
-			lesFonds[0].add(new JLabel(""+chHeros.chNom));
-			lesFonds[1].add(new JLabel(""+chHeros.chClasse));
-			lesFonds[2].add(new JLabel("Vie: "+chHeros.intToString(chHeros.chVie)));
-			lesFonds[3].add(new JLabel("Atk: "+chHeros.intToString(chHeros.chAtk)));
-			lesFonds[4].add(new JLabel("Def: "+chHeros.intToString(chHeros.chDef)));
-			lesFonds[5].add(new JLabel("Dgts: "+chHeros.intToString(chHeros.chDgts)));
-			}
-		}
-	public void actualiserFonds(PanelDuHaut parPanel)
-		{
-		for(int i=0; i<6; i++)
-			{
-			parPanel.add(lesFonds[i]);
-			}
+		GridLayout gl = new GridLayout();
+		gl.setColumns(1); gl.setRows(6); gl.setHgap(9); gl.setVgap(9);
+		setLayout(gl);
+		
+		
+		add(new JLabel("Nom: "+chHeros.chNom));
+		add(new JLabel("Classe: "+chHeros.chClasse));
+		add(new JLabel("Vie: "+chHeros.intToString(chHeros.chVie)));
+		add(new JLabel("Atk: "+chHeros.intToString(chHeros.chAtk)));
+		add(new JLabel("Def: "+chHeros.intToString(chHeros.chDef)));
+		add(new JLabel("Dgts: "+chHeros.intToString(chHeros.chDgts)));
+		
 		}
 	}
